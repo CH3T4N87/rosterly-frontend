dafb7d391788020c5c47734be833f607da162d81
Chetan Kshirsagar <chetankshirsagar87@gmail.com>
2025-09-08 11:26:31 +0530

    loading state added to dashboard

--- src/components/HomePage/Dashboard.jsx
+++ src/components/HomePage/Dashboard.jsx
@@ -7,35 +7,44 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const Home = () => {
   const [students, setStudents] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true); //loading state
     const fetchStudents = async () => {
       try {
         const res = await axios.get("https://rosterly-backend.onrender.com/student/getAll");
         setStudents(res.data);
       } catch (error) {
         console.error("Error fetching students:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchStudents();
-  }, []); 
-const handleDeleteUI = (id) => {
+  }, []);
+  const handleDeleteUI = (id) => {
     setStudents(students.filter((s) => s._id !== id));
     toast.success("🗑️ Student deleted successfully!");
   };
   return (
     <div className="dashboard">
       <Navbar />
-    <ToastContainer position="bottom-right" autoClose={3000} />
-      <div className="students-container">
-        {students.length > 0 ? (
-          students.map((student) => (
-            <Student key={student._id} student={student} onDelete={handleDeleteUI}/>
-          ))
+      <ToastContainer position="bottom-right" autoClose={3000} />
+      {
+        loading ? (
+          <div className="alert alert-warning text-center">Fetching....(Server is Starting..)</div>
         ) : (
-          <p>No students found</p>
-        )}
-      </div>
+          <div className="students-container">
+            {students.length > 0 ? (
+              students.map((student) => (
+                <Student key={student._id} student={student} onDelete={handleDeleteUI} />
+              ))
+            ) : (
+              <p>No students found</p>
+            )}
+          </div>
+        )
+      }
     </div>
   );
 };
